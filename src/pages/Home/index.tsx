05bf3f10c69e27038f5207c5bc3909226db45031
Emily <emilylemosn.u@gmail.com>
2025-06-16 18:51:09 -0300

    Projeto Efood iniciado

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+    return <h1>Página Inicial do efood</h1>
+}
+
+export default Home
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  body {
+    background-color: #fff;
+    color: #000;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`
+
+export default GlobalStyles
